00d7e2cf8124bbaa93c3a06e57748545862c3b3e
dcoberm <obermuellerdylan@live.com>
2020-08-22 11:27:56 -0500

    push

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def home():
 def covid():
     session = Session(engine)
 
-    covid = session.query(covid_stats.Zipcode, covid_stats.COVID19_Cases).order_by(covid_stats.Zipcode).all()
+    covid = session.query(covid_stats.Zipcode, covid_stats.COVID19_Cases).order_by(covid_stats.COVID19_Cases).all()
     crime= session.query(crime_stats.Zipcode, crime_stats.Report_No).order_by(crime_stats.Zipcode).all()
     pop= session.query(pop_stats.Zipcode, pop_stats.Population).order_by(pop_stats.Zipcode).all()
 
@@ -36,7 +36,7 @@ def covid():
         covid_dict['zipcode'] = Zipcode
         covid_dict['covid_cases'] = COVID19_Cases
         covid_list.append(covid_dict)
-        data_list.append(covid_list)
+    data_list.append(covid_list)
     
     crime_list = []
     for Zipcode, Report_No in crime:
@@ -44,7 +44,7 @@ def covid():
         crime_dict['zipcode'] = Zipcode
         crime_dict['crime_cases'] = Report_No
         crime_list.append(crime_dict)
-        data_list.append(crime_list)
+    data_list.append(crime_list)
 
     pop_list = []
     for Zipcode, Population in pop:
@@ -52,7 +52,7 @@ def covid():
         pop_dict['zipcode'] = Zipcode
         pop_dict['population'] = Population
         pop_list.append(pop_dict)
-        data_list.append(pop_list)
+    data_list.append(pop_list)
 
     return jsonify(data_list)
 
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,58 +1,60 @@
 
 // Creating map object
 var myMap = L.map("map", {
-    center: [39.0997, -94.5786],
-    zoom: 5
+    center: [39.0997, -94.5783],
+    zoom: 10
   });
   
-// Adding tile layer
-L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
-attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-maxZoom: 18,
-id: "mapbox.streets",
-accessToken: API_KEY
-}).addTo(myMap);
+  // Adding tile layer
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 650,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
 
-geoData = '../../Data/Zip Code.geojson'
+geoData = 'Data/Zip Codes.geojson'
 
-// Grab the data with d3
-d3.json(geoData, function(data) {
+// // Grab the data with d3
+// d3.json(geoData, function(data) {
 
-    // Create a new choropleth layer
-    choroplethLayer = L.choropleth(data, {
-      valueProperty: 'covid',
-      scale: ['lightyellow', 'gold', 'goldenrod', 'darkgoldenrod', 'sienna', 'saddlebrown'],
-      steps: 5,
-      mode: 'q',
-      style: {
-        color: '#fff',
-        weight: 1,
-        fillOpacity: 0.8
-      },
-      onEachFeature: function (feature, layer) {
-        layer.bindPopup('State: ' + feature.properties.NAME+ "<br> Breweries: " + feature.properties.brewery_count)
-      }
-    }).addTo(myMap)
+//     // Create a new choropleth layer
+//     choroplethLayer = L.choropleth(data, {
+//       valueProperty: 'covid',
+//       scale: ['lightyellow', 'gold', 'goldenrod', 'darkgoldenrod', 'sienna', 'saddlebrown'],
+//       steps: 5,
+//       mode: 'q',
+//       style: {
+//         color: '#fff',
+//         weight: 1,
+//         fillOpacity: 0.8
+//       },
+//       onEachFeature: function (feature, layer) {
+//         layer.bindPopup('State: ' + feature.properties.NAME+ "<br> Breweries: " + feature.properties.brewery_count)
+//       }
+//     }).addTo(myMap)
 
-    var legend = L.control({ position: 'bottomright' })
-    legend.onAdd = function (map) {
-      var div = L.DomUtil.create('div', 'info legend')
-      var limits = choroplethLayer.options.limits
-      var colors = choroplethLayer.options.colors
-      var labels = []
+//     var legend = L.control({ position: 'bottomright' })
+//     legend.onAdd = function (map) {
+//       var div = L.DomUtil.create('div', 'info legend')
+//       var limits = choroplethLayer.options.limits
+//       var colors = choroplethLayer.options.colors
+//       var labels = []
   
-      // Add min & max
-      div.innerHTML = '<h2>Number of COVID Cases</h2> <div class="labels"><div class="min">' + limits[0] + 
-      '</div> <div class="max">' + limits[limits.length - 1] + '</div></div>'
+//       // Add min & max
+//       div.innerHTML = '<h2>Number of COVID Cases</h2> <div class="labels"><div class="min">' + limits[0] + 
+//       '</div> <div class="max">' + limits[limits.length - 1] + '</div></div>'
   
-      limits.forEach(function (limit, index) {
-        labels.push('<li style="background-color: ' + colors[index] + '"></li>')
-      })
+//       limits.forEach(function (limit, index) {
+//         labels.push('<li style="background-color: ' + colors[index] + '"></li>')
+//       })
   
-      div.innerHTML += '<ul>' + labels.join('') + '</ul>'
-      return div
-    }
-    legend.addTo(myMap)
-  });
+//       div.innerHTML += '<ul>' + labels.join('') + '</ul>'
+//       return div
+//     }
+//     legend.addTo(myMap)
+//   });
   
 
