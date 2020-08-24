8236c9311e8a21e034f0f4b1858290c6ae590043
Jerod Rhodes <63427467+jerodrhodes@users.noreply.github.com>
2020-08-24 11:37:35 -0500

    scatter plot added to page

--- static/js/app.js
+++ static/js/app.js
@@ -1,49 +1,51 @@
 function buildPlot() {
     // Read in json 
-    d3.json('/data').then( function(data) {
+    d3.json('/data').then(function(data) {
         console.log(data)
         // Get covid cases and crime cases from file
-        var covid = data[0].covid_cases;
-        var crime = data[1].crime_cases;
-    })
+        var scatterData = [];
+        for (var i = 0; i <data[0].length; i++) {
+            for (var j = 0; j < data[1].length; j++) {
+                if (data[0][i].zipcode === data[1][j].zipcode) {
+                    scatterData.push({"zipcode": "_" + data[0][i].zipcode + "_", 
+                    "covid_cases": data[0][i].covid_cases,
+                    "crime_cases": data[1][j].crime_cases});
+                }
+            }
+        };
+        console.log(scatterData);
+        zipcode = scatterData.map(({ zipcode }) => zipcode)
+        console.log(zipcode);
+        covid_cases = scatterData.map(({ covid_cases }) => covid_cases)
+        console.log(covid_cases);
+        crime_cases = scatterData.map(({ crime_cases }) => crime_cases)
+        console.log(crime_cases);
 
+        var trace1 = {
+            x: covid_cases,
+            y: crime_cases,
+            mode: "scatter",
+            // marker:{
+            //         size: crime_cases,
+            //         color: covid_cases
+            //             },
+            text: zipcode
+          };
+    
+        var data1 = [trace1];
+        var layout1 = {
+              title: "Covid Cases vs. Crime Cases by Zipcode",
+              xaxis: {title: "Covid Cases"},
+              yaxis: {title: "Crime Cases"},
+            //   height: 600,
+            //   width: 1200
+          };
+    
+        Plotly.newPlot("img1", data1, layout1);
+    });
+
+    
+      
 }
 
-
-// function buildPlot() {
-//     // Read in json data
-//     d3.json("samples.json").then(bellydata => {
-//         console.log(bellydata)
-//         // Get OTU ids from sample file
-//         var otuIds = bellydata.samples[0].otu_ids;
-//         console.log(otuIds)
-//         // Get # of samples for top ten ids, reverse so h bar chart is correct
-//         var sampleValues = bellydata.samples[0].sample_values.slice(0,10).reverse();
-//         console.log(sampleValues)
-//         // Get labels for top 10 otu ids.
-//         var otuLabels = bellydata.samples[0].otu_labels.slice(0,10).reverse();
-//         console.log(`OTU labels: ${otuLabels}`)
-//         // get top 10 otu ids for the bar plot and reverse it
-//         var topOtuIds = bellydata.samples[0].otu_ids.slice(0,10).reverse();
-//         var otuIdLabels = topOtuIds.map(d => "OTU " + d);
-//         console.log(`OTU ids: ${otuIdLabels}`)
-
-// // Bubble Chart
-// var trace2 = {
-//     x: bellydata.samples[i].otu_ids,
-//     y: bellydata.samples[i].sample_values,
-//     mode: "markers",
-//     marker:{
-//         size: bellydata.samples[i].sample_values,
-//         color: bellydata.samples[i].otu_ids
-//     },
-//     text: bellydata.samples[i].otu_labels
-// };
-
-// var data2 = [trace2];
-// var layout1 = {
-//     title: "Patient OTU Counts",
-//     xaxis: {title: "OTU ID"},
-//     height: 600,
-//     width: 1200
-// };
+buildPlot();
