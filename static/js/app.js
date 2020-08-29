4a4c5198f019f9a60010a3c83cb04300b613edd6
Jerod Rhodes <63427467+jerodrhodes@users.noreply.github.com>
2020-08-28 22:12:11 -0500

    updated scatter plot to include population

--- static/js/app.js
+++ static/js/app.js
@@ -6,11 +6,14 @@ function buildPlot() {
         var scatterData = [];
         for (var i = 0; i <data[0].length; i++) {
             for (var j = 0; j < data[1].length; j++) {
-                if (data[0][i].zipcode === data[1][j].zipcode) {
-                    scatterData.push({"zipcode": "_" + data[0][i].zipcode + "_", 
-                    "covid_cases": data[0][i].covid_cases,
-                    "crime_cases": data[1][j].crime_cases});
-                }
+                for (var k = 0; k < data[2].length; k++) {
+                    if (data[0][i].zipcode === data[1][j].zipcode & data[0][i].zipcode === data[2][k].zipcode) {
+                        scatterData.push({"zipcode": "_" + data[0][i].zipcode + "_", 
+                        "covid_cases": data[0][i].covid_cases,
+                        "crime_cases": data[1][j].crime_cases,
+                        "population": data[2][k].population});
+                    }
+                }    
             }
         };
         console.log(scatterData);
@@ -20,29 +23,49 @@ function buildPlot() {
         console.log(covid_cases);
         crime_cases = scatterData.map(({ crime_cases }) => crime_cases)
         console.log(crime_cases);
+        population = scatterData.map(({ population }) => population)
+        console.log(population);
 
         var trace1 = {
             x: covid_cases,
             y: crime_cases,
             mode: "markers",
             type: "scatter",
-            // marker:{
-            //         size: 5,
-            //         color: covid_cases
-            //             },
-            text: zipcode
+            text: zipcode,
+            name: "Crime"
+          };
+
+        var trace2 = {
+            x: covid_cases,
+            y: population,
+            yaxis: 'y2',
+            mode: "markers",
+            type: "scatter",
+            color: "red",
+            text: zipcode,
+            name: "Pop."
           };
     
-        var data1 = [trace1];
+        var data1 = [trace1, trace2];
         var layout1 = {
-              title: "Covid Cases vs. Crime Cases by Zipcode",
-              xaxis: {title: "Covid Cases"},
-              yaxis: {title: "Crime Cases"},
-            //   height: 600,
-            //   width: 1200
-          };
+                title: "Covid Cases vs. Crime Cases & Population by Zipcode",
+                xaxis: {title: "Covid Cases"},
+                yaxis: {title: "Crime Cases"},
+                yaxis2: {
+                    title: "Population",
+                    overlaying: 'y',
+                    side: 'right'
+                },
+                legend: {
+                    orientation:"h",
+                    yanchor:"bottom",
+                    y:1.05,
+                    xanchor:"center",
+                    x:0.5
+                    }
+            }
+        Plotly.newPlot("img1", data1, layout1)
     
-        Plotly.newPlot("img1", data1, layout1);
     });
 
     
